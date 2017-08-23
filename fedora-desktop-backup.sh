30f4d4c6bc01c95d605e4e3bd3ebb3d7767c981b
Zachary Todd <zttodd.github@gmail.com>
2017-08-22 20:24:44 -0400

    Added backups, .ssh directories to exclude list.

--- fedora-desktop-backup.sh
+++ fedora-desktop-backup.sh
@@ -8,17 +8,19 @@
 DATE=`date +%Y%m%d`
 
 # change into root directory
-cd /
+cd ~/backups
 
 sudo tar -cvpzf $DATE.archive.tar.gz \
 --exclude=$HOME/.cache \
 --exclude=$HOME/.config/chromium \
 --exclude=$HOME/.config/google-chrome \
---exclude=$HOME/Dropbox \
 --exclude=$HOME/.dropbox \
 --exclude=$HOME/.dropbox-dist \
 --exclude=$HOME/.vagrant.d \
+--exclude=$HOME/.ssh \
 --exclude=$HOME/.var \
+--exclude=$HOME/backups \
+--exclude=$HOME/Dropbox \
 --exclude=$HOME/'VirtualBox VMs' \
 /etc \
 /home
